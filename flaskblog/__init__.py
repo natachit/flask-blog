cd9a6d5eca84fefdb6d5062a6476507f776fb50b
natachit <natachit@hotmail.com>
2018-09-16 01:12:37 +0700

    can push register to database with hashed password

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'eaf2b29a66444de11bb49ebad5dd14e2' #prevent modify cookie
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
